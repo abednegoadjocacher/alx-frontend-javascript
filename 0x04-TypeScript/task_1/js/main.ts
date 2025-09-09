dd08645fc48d266783e22b11d5cd8d0aa881d36c
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-09 22:37:43 +0000

    Extending the teacher interface to directors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,16 @@ const teacher: Teacher = {
   contract: false,
 };
 console.log(teacher);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
