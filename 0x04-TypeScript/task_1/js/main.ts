ec2099b057d210f039cc57f398a9bc396189d970
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-11 11:48:59 +0000

    A function that prints the first letter of the firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,28 +30,25 @@ const director1: Director = {
 };
 console.log(director1);
 
-// interface printTeacherFunction {
-//   firstName: string,
-//   lastName: string,
-// }
-// const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  //   return `${firstName.charAt(0)}. ${lastName}`;
-  // };
-  // function printTeacher(teachFullName: printTeacherFunction): string {
-  //   return `${teachFullName.firstName[0]}. ${teachFullName.lastName}`;
-  // }
-  // console.log(printTeacher({ firstName: 'John', lastName: 'Doe' }));
-
-// Interface definition
 interface printTeacherFunction {
-  (teacher: { firstName: string; lastName: string }): string;
+  (firstName: string, lastName: string): string;
 }
 
-// Function implementation
-const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
-  firstName = firstName.charAt(0);
-  return `${firstName}. ${lastName}`;
-};
+function printTeacher({firstName, lastName}: {firstName: string, lastName: string}): string {
+    return `${firstName[0]}. ${lastName}`;
+   }
+   console.log(printTeacher({ firstName: 'John', lastName: 'Doe' }));
 
-// Test
-console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+// Interface definition
+// interface printTeacherFunction {
+//   (teacher: { firstName: string; lastName: string }): string;
+// }
+
+// // Function implementation
+// const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
+//   firstName = firstName.charAt(0);
+//   return `${firstName}. ${lastName}`;
+// };
+
+// // Test
+// console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
