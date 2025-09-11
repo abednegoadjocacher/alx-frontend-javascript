8850449f6e9a190e589b6ad5f595fd5fea32b82a
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-11 10:24:32 +0000

    A function that prints the first letter of the firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -31,13 +31,14 @@ const director1: Director = {
 console.log(director1);
 
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  firstName: string,
+  lastName: string,
 }
 // const printTeacher: printTeacherFunction = (firstName, lastName) => {
   //   return `${firstName.charAt(0)}. ${lastName}`;
   // };
-  function printTeacher(firstName: string, lastName: string): string {
-    let firstLetter: string = firstName[0];
-    return `${firstLetter}. ${lastName}`;
+  function printTeacher(teachFullName: printTeacherFunction): string {
+    return `${teachFullName.firstName[0]}. ${teachFullName.lastName}`;
   }
-  console.log(printTeacher('John', 'Doe'));
+  console.log(printTeacher({ firstName: 'John', lastName: 'Doe' }));
+
