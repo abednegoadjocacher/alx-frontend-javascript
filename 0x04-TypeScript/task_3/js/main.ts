12e23e59ffcf0f406053a4bf5322595bce42a9ca
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-12 14:28:12 +0000

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,24 +3,14 @@
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud.js';
 
-// Create a row object
 const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
 };
 
-// Insert row
 const newRowID: RowID = CRUD.insertRow(row);
-// -> Insert row {firstName: "Guillaume", lastName: "Salva"}
 
-// Update row
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
-// -> Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
 
-// Delete row
 CRUD.deleteRow(newRowID);
-// -> Delete row id 125
